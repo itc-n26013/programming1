ab0cef27d3129c9b9cfb66cf958c85cdae23ec7d
itc-n26013 <n26013@std.it-college.ac.jp>
2026-07-15 01:55:42 +0000

    4-2

---
+++ chap04/q4_2_1.py
@@ -0,0 +1,15 @@
+def number_to_day(num=100):
+    if num == 0:
+        day='今日'
+    elif num == 1:
+        day='明日'
+    elif num==-1:
+        day='昨日'
+    else :
+        day='今日より1日を超えて離れた日'
+    return day
+
+print(number_to_day(0))
+print(number_to_day(1))
+print(number_to_day(-1))
+print(number_to_day(10))
---
+++ chap04/q4_2_2.py
@@ -0,0 +1,9 @@
+def per(m=100):
+    a,b,c=3,0,2
+    result=[]
+    while a < m:
+        result.append(a)
+        a,b,c=b,c,a+b
+    return result
+
+print(per())
